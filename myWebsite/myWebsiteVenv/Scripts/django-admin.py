4bba0b206695165c8c88c3a1afb099ecc51c1c3d
sushantdesaur <sushantdesaur@live.in>
2020-07-03 20:06:22 +0530

    second commit

---
+++ myWebsite/myWebsiteVenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ekans\Documents\Web Development\myWebsite\myWebsite\myWebsiteVenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
